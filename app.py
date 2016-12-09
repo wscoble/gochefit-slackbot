7c3caf684d14b12b60894109ac8c3c88e96679de
Scott Scoble <scott@scoble.tech>
2016-12-08 21:05:46 -0800

    clean up

--- app.py
+++ app.py
@@ -10,37 +10,36 @@ from base64 import b64decode
 from chalice import Chalice, ForbiddenError, BadRequestError
 from chalicelib import commands
 
-de = find_dotenv()
-if de is not None:
-    load_dotenv(de)
 
 app = Chalice(app_name='gochefit-slackbot')
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 app.debug = True
 
-slack_token = os.environ.get('SLACK_TOKEN')
 
+de = find_dotenv()
+if de is not None:
+    load_dotenv(de)
+
+slack_token = os.environ.get('SLACK_TOKEN')
 
 if os.environ.get('PROD') is not None:
     slack_token = boto3.client('kms').decrypt(
                         CiphertextBlob=b64decode(slack_token)
                     )['Plaintext']
 
+
 @app.route('/', methods=['POST'], content_types=['application/x-www-form-urlencoded'])
-def index():
+def handle_slack_command():
     body = urlparse.parse_qs(app.current_request.raw_body)
 
-    if 'token' not in body:
+    if 'token' not in body or 'text' not in body:
         raise BadRequestError
 
     token = body['token'][0]
 
     if token != slack_token:
         raise ForbiddenError("Invalid token")
-
-    if 'text' not in body:
-        raise BadRequestError
 
     command = body['text'][0]
 
